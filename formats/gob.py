bd424a67371fc537acbff1eaa1387e9c088b14b2
njankowski <njankowski@users.noreply.github.com>
2018-12-30 22:32:17 -0500

    Add more sophisticated filename validation.
    - Replace is_valid_entry_name() with is_valid_dos_name()
    - Remove GOB_INDEX_ENTRY_NAME_LENGTH

--- formats/gob.py
+++ formats/gob.py
@@ -1,3 +1,4 @@
+import string
 import struct
 
 
@@ -5,22 +6,56 @@ GOB_HEADER_SIZE = 8
 GOB_INDEX_SIZE = 4
 GOB_INDEX_ENTRY_SIZE = 21
 
-GOB_INDEX_ENTRY_NAME_LENGTH = 13
-
 GOB_MAX_SIZE = 2 ** 31 - 1
 
 GOB_MIN_INDEX_OR_DATA_OFFSET = GOB_HEADER_SIZE
 
 
-def is_valid_entry_name(name):
-    if not (0 < len(name) < GOB_INDEX_ENTRY_NAME_LENGTH):
+def is_valid_dos_name(filename):
+    """Return whether a string is a valid DOS filename"""
+    allowed = string.ascii_letters + string.digits + "_^$~!#%&-{}@`'()"
+    reserved = ['CON', 'PRN', 'AUX', 'CLOCK$', 'NUL', 'COM0', 'COM1', 'COM2',
+                'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9', 'LPT0',
+                'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8',
+                'LPT9', 'LST', 'KEYBD$', 'SCREEN$', '$IDLE$', 'CONFIG$']
+
+    # Filename cannot be empty or None.
+    if not filename:
         return False
-    try:
-        name.encode('ascii')
-    except UnicodeEncodeError:
+
+    # Period cannot appear more than once.
+    if not filename.count('.') <= 1:
         return False
-    else:
-        return True
+
+
+    s = filename.partition('.')
+    name = s[0]
+
+    # Name can be up to eight characters.
+    name_len_ok = (0 < len(name) <= 8)
+    # Name consists of only allowed characters.
+    name_char_ok = all(c in allowed for c in name)
+    # Name cannot be reserved.
+    name_reserved_ok = (name.upper() not in reserved)
+
+    if not (name_len_ok and name_char_ok and  name_reserved_ok):
+        return False
+
+
+    extension = s[2]
+
+    # Extension can be excluded.
+    if extension:
+        # Extension can be up to three characters.
+        ext_len_ok = (0 <= len(extension) <= 3)
+        # Name consists of only allowed characters.
+        ext_char_ok = all(c in allowed for c in name)
+
+        if not (ext_len_ok and ext_char_ok):
+            return False
+
+
+    return True
 
 
 def get_gob_size(entries):
@@ -73,7 +108,7 @@ def write(filename, entries):
         raise Exception('entries cannot fit into GOB')
 
     for entry in entries:
-        if not is_valid_entry_name(entry[0]):
+        if not is_valid_dos_name(entry[0]):
             raise Exception('"' + entry[0] + '" is an invalid filename.')
 
     with open(filename, 'wb') as file:
