57828b4d222f6adb2c3c432eeef8a05aa1c7cc6b
njankowski <njankowski@users.noreply.github.com>
2020-01-20 20:31:11 -0500

    Cleanup GOB and LAB source files.
    - Add docstrings
    - Rename uses of 'index' in GOB context to 'catalog' to avoid confusion
    - Remove redundant GOB_MIN_INDEX_OR_DATA_OFFSET constant
    - Add generic exception classes for GOB and LAB
    - Rename gob.is_valid_dos_name() to gob.is_valid_entry_name()
    - Invert separator check in gob.is_valid_entry_name()
    - Check extension rules in gob.is_valid_entry_name() if separator is present and
    not just if extension is present
    - Rename short variables with additional information
    - Add lab.is_valid_entry_name()
    - Add lab.get_lab_size()
    - Move lab.read_until_null() into a subfunction in lab.read()
    - Fix logic mistake in lab.read() where offset was used instead of length when
    deciding whether to seek for data
    - Read entry name in lab.read() regardless of data length
    - Check name table length in lab.read()
    - Add lab.write()
    - Rewrite miscellaneous sections for clarity or simplification

--- formats/gob.py
+++ formats/gob.py
@@ -1,100 +1,121 @@
+"""
+Star Wars: Dark Forces
+GOB Container Functions
+"""
 import string
 import struct
 
 
 GOB_HEADER_SIZE = 8
-GOB_INDEX_SIZE = 4
-GOB_INDEX_ENTRY_SIZE = 21
+GOB_CATALOG_OFFSET_SIZE = 4
+GOB_CATALOG_ENTRY_SIZE = 21
 
 GOB_MAX_SIZE = 2 ** 31 - 1
 
-GOB_MIN_INDEX_OR_DATA_OFFSET = GOB_HEADER_SIZE
+
+class GOBException(Exception):
+    pass
 
 
-def is_valid_dos_name(filename):
-    """Return whether a string is a valid DOS filename"""
+def is_valid_entry_name(filename):
+    """Return whether a name is valid as an entry name.
+
+    Checks a name against an assortment of DOS-like filename rules.
+
+    :param filename: The name to check
+    :return: bool
+    """
     allowed = string.ascii_letters + string.digits + "_^$~!#%&-{}@`'()"
-    reserved = ['CON', 'PRN', 'AUX', 'CLOCK$', 'NUL', 'COM0', 'COM1', 'COM2',
-                'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9', 'LPT0',
-                'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8',
-                'LPT9', 'LST', 'KEYBD$', 'SCREEN$', '$IDLE$', 'CONFIG$']
+    reserved = ['CON', 'PRN', 'AUX', 'CLOCK$', 'NUL',
+                'COM0', 'COM1', 'COM2', 'COM3', 'COM4', 'COM5', 'COM6', 'COM7', 'COM8', 'COM9',
+                'LPT0', 'LPT1', 'LPT2', 'LPT3', 'LPT4', 'LPT5', 'LPT6', 'LPT7', 'LPT8', 'LPT9',
+                'LST', 'KEYBD$', 'SCREEN$', '$IDLE$', 'CONFIG$']
 
-    # Filename cannot be empty or None.
+    # Cannot be empty or None.
     if not filename:
         return False
 
-    # Period cannot appear more than once.
-    if not filename.count('.') <= 1:
+    # Separator cannot appear more than once.
+    if filename.count('.') > 1:
         return False
 
-
+    # Split into name and extension.
     s = filename.partition('.')
     name = s[0]
-
-    # Name can be up to eight characters.
-    name_len_ok = (0 < len(name) <= 8)
-    # Name consists of only allowed characters.
-    name_char_ok = all(c in allowed for c in name)
-    # Name cannot be reserved.
-    name_reserved_ok = (name.upper() not in reserved)
-
-    if not (name_len_ok and name_char_ok and  name_reserved_ok):
-        return False
-
-
+    separator = s[1]
     extension = s[2]
 
-    # Extension can be excluded.
-    if extension:
-        # Extension can be up to three characters.
-        ext_len_ok = (0 <= len(extension) <= 3)
-        # Name consists of only allowed characters.
+    # Check name length.
+    name_len_ok = (0 < len(name) <= 8)
+    # Check name characters.
+    name_char_ok = all(c in allowed for c in name)
+    # Check name reservation.
+    name_reserved_ok = (name.upper() not in reserved)
+
+    # Default to valid extension checks.
+    ext_len_ok = True
+    ext_char_ok = True
+
+    # Check extension if a separator is present.
+    # Must have a valid extension if separator is present.
+    if separator:
+        # Check extension length.
+        ext_len_ok = (0 < len(extension) <= 3)
+        # Check extension characters.
         ext_char_ok = all(c in allowed for c in name)
-
-        if not (ext_len_ok and ext_char_ok):
-            return False
+        # Reserved names do not apply to extensions.
 
 
-    return True
+    return ((name_len_ok and name_char_ok and  name_reserved_ok) and (ext_len_ok and ext_char_ok))
 
 
 def get_gob_size(entries):
-    meta_size = (GOB_HEADER_SIZE + GOB_INDEX_SIZE +
-            GOB_INDEX_ENTRY_SIZE * len(entries))
+    """Return a tuple of size information given a list of entries.
 
-    data_size = 0
-    for entry in entries:
-        data_size += len(entry[1])
+    Projects the meta data size and raw data size of a GOB if it were created with the given list of entries.
+
+    :param entries: List of GOB entry tuples [(str, bytes), ..., ] where the tuple represents (name, data) of the entry
+    :return: A tuple containing meta data size and raw data size in number of bytes (meta_size, data_size)
+    """
+    # Header + Catalog Offset + Catalog
+    meta_size = GOB_HEADER_SIZE + GOB_CATALOG_OFFSET_SIZE + (GOB_CATALOG_ENTRY_SIZE * len(entries))
+    # Raw Data
+    data_size = sum([len(entry[1]) for entry in entries])
 
     return (meta_size, data_size)
 
 
 def read(filename):
+    """Reads a GOB container and returns all stored files.
+
+    :param filename: Path to the GOB to read
+    :return: List of GOB entry tuples [(str, bytes), ..., ] where the tuple represents (name, data) of the entry
+    """
     with open(filename, 'rb') as file:
         entries = []
 
         if file.read(4) != b'GOB\n':
             return
 
-        index_offset = struct.unpack('<i', file.read(4))[0]
+        catalog_offset = struct.unpack('<i', file.read(4))[0]
 
-        file.seek(index_offset)
+        file.seek(catalog_offset)
         num_entries = struct.unpack('<i', file.read(4))[0]
 
         for i in range(num_entries):
-            offset = struct.unpack('<i', file.read(4))[0]
-            length = struct.unpack('<i', file.read(4))[0]
+            data_offset = struct.unpack('<i', file.read(4))[0]
+            data_length = struct.unpack('<i', file.read(4))[0]
 
             raw_name = file.read(13)
             name = raw_name[0 : raw_name.index(0)].decode('ascii')
 
             data = b''
 
-            if length > 0:
+            if data_length > 0:
                 next_entry = file.tell()
 
-                file.seek(offset)
-                data = file.read(length)
+                file.seek(data_offset)
+                data = file.read(data_length)
                 file.seek(next_entry)
 
             entries.append((name, data))
@@ -103,25 +124,31 @@ def read(filename):
 
 
 def write(filename, entries):
+    """Writes a GOB container given a path and a list of GOB entries.
+
+    :param filename: Path to write the GOB to
+    :param entries: List of GOB entry tuples [(str, bytes), ..., ] where the tuple represents (name, data) of the entry
+    :return: None
+    """
     meta_size, data_size = get_gob_size(entries)
     if (meta_size + data_size) > GOB_MAX_SIZE:
-        raise Exception('entries cannot fit into GOB')
+        raise GOBException('Cannot create GOB because it would exceed maximum size.')
 
     for entry in entries:
-        if not is_valid_dos_name(entry[0]):
-            raise Exception('"' + entry[0] + '" is an invalid filename.')
+        if not is_valid_entry_name(entry[0]):
+            raise GOBException('"' + entry[0] + '" is an invalid entry name.')
 
     with open(filename, 'wb') as file:
         file.write(b'GOB\n')
 
-        file.write(struct.pack('<i', GOB_MIN_INDEX_OR_DATA_OFFSET + data_size))
+        file.write(struct.pack('<i', GOB_HEADER_SIZE + data_size))
 
         for entry in entries:
             file.write(entry[1])
 
         file.write(struct.pack('<i', len(entries)))
 
-        offset = GOB_MIN_INDEX_OR_DATA_OFFSET
+        offset = GOB_HEADER_SIZE
         for entry in entries:
             file.write(struct.pack('<i', offset))
             file.write(struct.pack('<i', len(entry[1])))
--- formats/lab.py
+++ formats/lab.py
@@ -1,25 +1,62 @@
+"""
+Outlaws
+LAB Container Functions
+"""
 import struct
 
 
 LAB_HEADER_SIZE = 16
 LAB_CATALOG_ENTRY_SIZE = 16
+
 LAB_MAX_SIZE = 2 ** 31 - 1
 
 
-def get_lab_size(entries):
+class LABException(Exception):
     pass
 
 
-def read_until_null(file):
-    s = bytearray()
-    c = file.read(1)
-    while c != b'\x00':
-        s.extend(c)
-        c = file.read(1)
-    return bytes(s)
+def is_valid_entry_name(filename):
+    """Return whether a name is valid as an entry name.
+
+    :param filename: The name to check
+    :return: bool
+    """
+    # Fix Me
+    return True
+
+
+def get_lab_size(entries):
+    """Return a tuple of size information given a list of entries.
+
+    Projects the meta data size and raw data size of a LAB if it were created with the given list of entries.
+
+    :param entries: List of LAB entry tuples [(str, bytes), ..., ] where the tuple represents (name, data) of the entry
+    :return: A tuple containing meta data size and raw data size in number of bytes (meta_size, data_size)
+    """
+    # Header + Catalog + Name Table
+    meta_size = LAB_HEADER_SIZE + (LAB_CATALOG_ENTRY_SIZE * len(entries)) + (sum([(len(entry[0]) + 1) for entry in entries]))
+    # Raw Data
+    data_size = sum([len(entry[1]) for entry in entries])
+
+    return (meta_size, data_size)
 
 
 def read(filename):
+    """Reads a LAB container and returns all stored files.
+
+    :param filename: Path to the LAB to read
+    :return: List of LAB entry tuples [(str, bytes), ..., ] where the tuple represents (name, data) of the entry
+    """
+    # Utility function for reading null-terminated names.
+    def read_until_null(file):
+        s = bytearray()
+        c = file.read(1)
+        while c != b'\x00':
+            s.extend(c)
+            c = file.read(1)
+        return bytes(s)
+
+
     with open(filename, 'rb') as file:
         entries = []
 
@@ -42,23 +79,59 @@ def read(filename):
             data_length = struct.unpack('<i', file.read(4))[0]
             fourcc = file.read(4).decode('ascii')
 
+            next_entry = file.tell()
+            file.seek(name_table_start + name_offset)
+            name = read_until_null(file).decode('ascii')
+
             data = b''
 
-            if data_offset > 0:
-                next_entry = file.tell()
-
-                file.seek(name_table_start + name_offset)
-                name = read_until_null(file).decode('ascii')
-
+            if data_length > 0:
                 file.seek(data_offset)
                 data = file.read(data_length)
 
-                file.seek(next_entry)
+            file.seek(next_entry)
 
             entries.append((name, data))
+
+        if sum([(len(entry[0]) + 1) for entry in entries]) != name_table_len:
+            return
 
         return entries
 
 
 def write(filename, entries):
-    pass
+    """Writes a LAB container given a path and a list of LAB entries.
+
+    :param filename: Path to write the LAB to
+    :param entries: List of LAB entry tuples [(str, bytes), ..., ] where the tuple represents (name, data) of the entry
+    :return: None
+    """
+    meta_size, data_size = get_lab_size(entries)
+    if (meta_size + data_size) > LAB_MAX_SIZE:
+        raise LABException('Cannot create LAB because it would exceed maximum size.')
+
+    for entry in entries:
+        if not is_valid_entry_name(entry[0]):
+            raise LABException('"' + entry[0] + '" is an invalid entry name.')
+
+    with open(filename, 'wb') as file:
+        file.write(b'LABN')
+        file.write(struct.pack('<i', 0x00010000))
+        file.write(struct.pack('<i', len(entries)))
+        name_table_len = sum([(len(entry[0]) + 1) for entry in entries])
+        file.write(struct.pack('<i', name_table_len))
+
+        name_offset = 0
+        data_offset = LAB_HEADER_SIZE + (LAB_CATALOG_ENTRY_SIZE * len(entries)) + name_table_len
+        for entry in entries:
+            file.write(struct.pack('<i', name_offset))
+            file.write(struct.pack('<i', data_offset))
+            file.write(struct.pack('<i', len(entry[1])))
+            # Fix Me
+            file.write(struct.pack('<i', 0)))
+
+            name_offset += len(entry[0]) + 1
+            data_offset += len(entry[1])
+
+        for entry in entries:
+            file.write(entry[1])
