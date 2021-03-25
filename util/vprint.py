31b7ef8db4cfbfd9253fd70b9a82a68277708162
njankowski <njankowski@users.noreply.github.com>
2021-03-25 15:32:39 -0400

    Add CheckedAccessFileContext class.
    - Add file argument to vprint()
    - Adjust comments

--- util/vprint.py
+++ util/vprint.py
@@ -1,5 +1,6 @@
 from inspect import currentframe
 from os.path import basename, splitext
+from sys import stderr, stdout
 
 
 verbose = False
@@ -10,10 +11,10 @@ def set_verbose(enabled):
     verbose = enabled
 
 
-def vprint(message):
+def vprint(message, file=stdout):
     if verbose:
         caller_current_frame = currentframe()
         caller_path = caller_current_frame.f_back.f_code.co_filename
         caller_fuction_name = caller_current_frame.f_back.f_code.co_name
         caller_filename = splitext(basename(caller_path))[0]
-        print(f'{caller_filename}:{caller_fuction_name}: {message}')
+        print(f'{caller_filename}:{caller_fuction_name}: {message}', file=file)
